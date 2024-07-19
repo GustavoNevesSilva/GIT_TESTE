123ad243d33c2c996d111abc51969bc82763ec67
GustavoNevesSilva <nevess.gustavo7@gmail.com>
2024-07-19 16:04:28 -0300

    v2
    v2

---
+++ teste5.py
@@ -0,0 +1,12 @@
+bairro = 'Jardim Colorado'
+cidade = 'sao Paulo'
+estado = 'Sao Paulo'
+cep =  12333348
+
+
+print(bairro)
+print(cidade)
+print(estado)
+print(cep)
+      
+print("Meu estado é: ", estado)
